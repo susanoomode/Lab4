acbde0cebacaa5f2d349b79f6087745f767f866f
Danial Rehman <danial.rehman36@gmail.com>
2022-04-25 18:45:21 +0300

    Revert "Added one more line"
    This reverts commit a722ffac23771516f82707ff494bbce3858d915a.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("This lab is only just for practice with Git");
-    System.out.println("Another line for testing");
     }
 }
